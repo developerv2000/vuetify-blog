1fe48570ecccd56e1cd764e286fc0f8f4913c615
MrBobPRO <bobnuridinov@gmal.com>
2025-04-30 08:50:48 +0500

    SSR clustering added

--- resources/js/frontend/ssr.js
+++ resources/js/frontend/ssr.js
@@ -2,5 +2,6 @@ import createServer from '@inertiajs/vue3/server';
 import { initializeSSR } from '@/shared/appSetup';
 
 createServer((page) =>
-    initializeSSR(page, import.meta.globEager('./pages/**/*.vue'))
+    initializeSSR(page, import.meta.globEager('./pages/**/*.vue')),
+    { cluster: true }, // enable multiple processes
 );
